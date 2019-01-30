374b25c5b6b3f44cc490581f8c313c144666b44b
luigisamurai <labs.sistemas@gmail.com>
2019-01-30 11:28:09 -0500

    feat(project) add protractor retry to execute again the failed test

--- protractor.conf.ts
+++ protractor.conf.ts
@@ -1,9 +1,13 @@
 import { browser, Config } from 'protractor';
+import { retry } from 'protractor-retry';
 import { FileDetector } from 'selenium-webdriver/remote';
 import { appConfig } from './config';
 
 export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
+  afterLaunch: () => {
+    return retry.afterLaunch(appConfig.retryFailedTest);
+  },
   baseUrl: appConfig.baseUrl,
   cucumberOpts: {
     'format': [
@@ -16,11 +20,15 @@ export const config: Config = {
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
   getPageTimeout: appConfig.timeouts.long,
+  onCleanUp: (results) => {
+     retry.onCleanUp(results);
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().window().maximize();
     browser.manage().timeouts().implicitlyWait(appConfig.timeouts.implicitlyWait);
     browser.setFileDetector(new FileDetector());
+    retry.onPrepare();
   },
   specs: [
     '../test/features/**/*.feature'
